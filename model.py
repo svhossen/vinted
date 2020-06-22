666ce4b03cd787c2f83204fe7cdc54decb047237
svhossen <60427495+svhossen@users.noreply.github.com>
2020-06-22 14:15:05 +0200

    Your Message

--- model.py
+++ model.py
@@ -50,13 +50,7 @@ status =data.groupby('status').agg({'days_unsold': descriptive_statistics})
 
 summary_stats = pd.concat([country_code,listing_quality_string,listings_in_first_7days_detailed,brand_is_verified,listing_platform,status],keys = ['country_code','listing_quality_string','listings_in_first_7days_detailed','brand_is_verified','listing_platform','status'])
 
-#%%exporting results to an xlsx file
 
-# writer = pd.ExcelWriter('Report_python.xlsx', engine='xlsxwriter')
-# data_daily.to_excel(writer, 'data_daily')
-# data_group_unsold.to_excel(writer, 'data_group_unsold')
-
-# writer.save()
 
 #%%model trial 1
 '''
@@ -64,38 +58,38 @@ Potential model: ZeroInflatedNegativeBinomialP
 
 This model is adequate for categrical datasets with a relatively high frequency of zeros in the dependant variable
 '''
-# =============================================================================
 # import statsmodels
 # data['endog'] = data.loc[:,'days_unsold']
-# 
+
 # data_onehot = pd.get_dummies(data['listing_quality_string'])
-# 
+
 # mod_nb = statsmodels.discrete.count_model.ZeroInflatedNegativeBinomialP(data['endog'],data_onehot).fit()
-# 
+
 # print(mod_nb.summary())
-# 
+
 # mod_ols = sm.NegativeBinomial(data.days_unsold , data_onehot , data=data)
 # res_ols = mod_ols.fit(disp=False)
 # print(res_ols.summary())
-# 
+
+# mod_ols = sm.GLM(data.days_unsold , data_onehot , data=data)
+# res_ols = mod_ols.fit(disp=False)
+# print(res_ols.summary())
+
 # mod_nbin = sm.NegativeBinomial.from_formula('days_unsold ~ C(country_code) + C(listing_platform)+ C(brand_is_verified) + C(listing_quality_string) + C(status) + gmv_eur_fixed ', data=data)
 # res_nbin = mod_nbin.fit(disp=False)
 # print(res_nbin.summary())
-# =============================================================================
 
 #%%model 2
-['portal', 'id', 'created_at', 'user_id', 'listing_price_eur_fixed',
-       'status', 'sale_time', 'gmv_eur_fixed', 'brand', 'brand_is_verified',
-       'declined_at', 'color_primary', 'listing_quality_string',
-       'suggested_price_maximum', 'catalog_code_1', 'catalog_code_2',
-       'catalog_code_3', 'catalog_code_4', 'catalog_code_5', 'gender',
-       'country_code', 'lister_nth_listing', 'first_listing_local_date',
-       'listing_platform', 'registration_platform', 'registration_local_date',
-       'total_positive_feedback_count', 'total_negative_feedback_count',
-       'window_items_listed', 'window_items_bought', 'window_items_sold',
-       'listings_in_first_7days_detailed']
 
-mod_ols = smf.ols(formula = 'days_unsold ~ C(country_code) + C(listing_platform)+ C(brand_is_verified) + C(listing_quality_string) + window_items_sold + catalog_code_1 +status', data=data)
+
+# mod_ols = sm.OLS.from_formula(formula = 'days_unsold ~ C(country_code) + C(listing_platform)+ C(brand_is_verified) + C(listing_quality_string) + window_items_sold + catalog_code_1 +status', data=data)
+'''
+This model automatically create dummies for categorical data: C()
+
+'''
+
+
+mod_ols = sm.OLS.from_formula(formula = 'days_unsold ~ C(country_code) + C(listing_platform)+ C(brand_is_verified)  + window_items_sold + (catalog_code_1) +  C(listing_quality_string)+C(status) + listing_price_eur_fixed', data=data)
 
 print(mod_ols.fit().params)
 
@@ -106,7 +100,43 @@ print(mod_ols.fit().summary())
 results_as_html = mod_ols.fit().summary().tables[1].as_html()
 test = pd.read_html(results_as_html, header=0, index_col=0)[0]
 
-#%%
+#%%exporting results to an xlsx file
+
+# writer = pd.ExcelWriter('Report_python.xlsx', engine='xlsxwriter')
+# data_daily.to_excel(writer, 'data_daily')
+# data_group_unsold.to_excel(writer, 'data_group_unsold')
+# test.to_excel(writer, 'Regression_results')
+# summary_stats.to_excel(writer, 'summary_stats')
+
+# writer.save()
+
+
+  #%%
+# =============================================================================
+# data_nonan = data.dropna()
+# 
+# 
+# Y = data_nonan['days_unsold']
+# 
+# X = data_nonan[['country_code','listing_platform','brand_is_verified','listing_quality_string','catalog_code_1','status']]
+# 
+# X = pd.get_dummies(data=X, drop_first=True)
+# 
+# mod_bin = sm.NegativeBinomial(Y, X , data=data_nonan)
+# res_bin = mod_bin.fit(disp=False)
+# print(res_bin.summary())
+# 
+# 
+# 
+# 
+# from sklearn import linear_model
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = .20, random_state = 40)
+# regr = linear_model.LinearRegression() # Do not use fit_intercept = False if you have removed 1 column after dummy encoding
+# regr.fit(X_train, Y_train)
+# predicted = regr.predict(X_test)
+# =============================================================================
+
 
 
 
